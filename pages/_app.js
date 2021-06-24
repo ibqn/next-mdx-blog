291b4bf7ba2b851860ce9198ed6ea82c8f46fe86
ibqn <evgen.ibqn@gmail.com>
2021-06-24 12:16:51 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
 import '../styles/index.css'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const styled = { createGlobalStyle }
+
+const GlobalStyle = styled.createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
